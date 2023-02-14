170809ef41b8621ec0b17f4a46ee9bc084caf423
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 17:30:53 -0300

    Alterando a classe NotificadorSMS para implementar a interface.

--- src/main/java/com/lsilva/matheus/notificacao/NotificadorSMS.java
+++ src/main/java/com/lsilva/matheus/notificacao/NotificadorSMS.java
@@ -2,7 +2,8 @@ package com.lsilva.matheus.notificacao;
 
 import com.lsilva.matheus.modelo.Cliente;
 
-public class NotificadorSMS {
+public class NotificadorSMS implements Notificador {
+    @Override
     public void notificar(Cliente cliente, String mensagem) {
         System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
                 cliente.getNome(), cliente.getTelefone(), mensagem);
