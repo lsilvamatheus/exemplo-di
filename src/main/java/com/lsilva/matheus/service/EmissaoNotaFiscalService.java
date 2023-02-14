254b12015e0fa980591353949a09bbc535e64089
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 17:49:32 -0300

    Alterando a classe EmissaoNotaFiscalService para receber uma instância do objeto Notificador via construtor

--- src/main/java/com/lsilva/matheus/service/EmissaoNotaFiscalService.java
+++ src/main/java/com/lsilva/matheus/service/EmissaoNotaFiscalService.java
@@ -2,13 +2,17 @@ package com.lsilva.matheus.service;
 
 import com.lsilva.matheus.modelo.Cliente;
 import com.lsilva.matheus.modelo.Produto;
-import com.lsilva.matheus.notificacao.NotificadorSMS;
+import com.lsilva.matheus.notificacao.Notificador;
 
 public class EmissaoNotaFiscalService {
+    private final Notificador notificador;
+
+    public EmissaoNotaFiscalService(Notificador notificador) {
+        this.notificador = notificador;
+    }
 
     public void emitir(Cliente cliente, Produto produto) {
         //TODO emite a nota fiscal aqui...
-        NotificadorSMS notificador = new NotificadorSMS();
         notificador.notificar(cliente,
                 "Nota fiscal do produto "
                 + produto.getNome() + " foi emitida!");
