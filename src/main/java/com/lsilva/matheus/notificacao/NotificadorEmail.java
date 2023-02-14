b4f9a4934e16e4466da67617752f7ac8b2d9655f
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 17:30:35 -0300

    Alterando a classe NotificadorEmail para implementar a interface.

--- src/main/java/com/lsilva/matheus/notificacao/NotificadorEmail.java
+++ src/main/java/com/lsilva/matheus/notificacao/NotificadorEmail.java
@@ -2,7 +2,8 @@ package com.lsilva.matheus.notificacao;
 
 import com.lsilva.matheus.modelo.Cliente;
 
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
+    @Override
     public void notificar(Cliente cliente, String mensagem) {
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
         cliente.getNome(), cliente.getEmail(), mensagem);
