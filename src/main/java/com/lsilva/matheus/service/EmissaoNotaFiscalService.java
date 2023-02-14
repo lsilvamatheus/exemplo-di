e29074e37114110fad88f621f79d30ea0a6af99c
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-14 17:21:25 -0300

    Alterando classe de serviço de emissão de nota fiscal para utilizar o notificador via SMS

--- src/main/java/com/lsilva/matheus/service/EmissaoNotaFiscalService.java
+++ src/main/java/com/lsilva/matheus/service/EmissaoNotaFiscalService.java
@@ -2,13 +2,13 @@ package com.lsilva.matheus.service;
 
 import com.lsilva.matheus.modelo.Cliente;
 import com.lsilva.matheus.modelo.Produto;
-import com.lsilva.matheus.notificacao.NotificadorEmail;
+import com.lsilva.matheus.notificacao.NotificadorSMS;
 
 public class EmissaoNotaFiscalService {
 
     public void emitir(Cliente cliente, Produto produto) {
         //TODO emite a nota fiscal aqui...
-        NotificadorEmail notificador = new NotificadorEmail();
+        NotificadorSMS notificador = new NotificadorSMS();
         notificador.notificar(cliente,
                 "Nota fiscal do produto "
                 + produto.getNome() + " foi emitida!");
