66f695cdc43246cfca5d999c52688f56c90cdcad
lsilvamatheus <lsilva.matheus@gmail.com>
2023-02-15 10:49:11 -0300

    Alterando a classe Main para instanciar e injetar a dependência da classe de notificação nas classes de serviço.

--- src/main/java/com/lsilva/matheus/Main.java
+++ src/main/java/com/lsilva/matheus/Main.java
@@ -1,6 +1,8 @@
 package com.lsilva.matheus;
 
 import com.lsilva.matheus.modelo.Cliente;
+import com.lsilva.matheus.notificacao.Notificador;
+import com.lsilva.matheus.notificacao.NotificadorSMS;
 import com.lsilva.matheus.service.AtivacaoClienteService;
 
 public class Main {
@@ -8,7 +10,8 @@ public class Main {
         Cliente joao = new Cliente("João", "joao@xyz.com", "19999999999");
         Cliente maria = new Cliente("Maria", "maria@xyz.com", "19888888888");
 
-        AtivacaoClienteService service = new AtivacaoClienteService();
+        Notificador notificador = new NotificadorSMS();
+        AtivacaoClienteService service = new AtivacaoClienteService(notificador);
         service.ativar(joao);
         service.ativar(maria);
     }
